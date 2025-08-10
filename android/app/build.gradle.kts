e24e1fc2f370f96ffd0b8bb4a53decf88311c7fe
clever akanimoh <cleverakanimoh02@gmail.com>
2025-08-10 20:12:52 +0100

    modified build.gradle.kts in app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.freedom.app.freedom"
+        applicationId = "com.freedomghana.app.user"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
