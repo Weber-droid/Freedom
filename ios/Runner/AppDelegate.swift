91f695390a48e429443c0de43a8a4c5f91434903
maxiggle <clsyfriday@gmail.com>
2025-11-10 10:27:40 +0100

    chore:changed luncher icon images

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,25 +8,33 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let dartDefinesString = Bundle.main.infoDictionary!["DART_DEFINES"] as! String
-    print("my key \(dartDefinesString)")
+    guard let dartDefinesString = Bundle.main.infoDictionary?["DART_DEFINES"] as? String else {
+      print("DART_DEFINES not found in Info.plist")
+      GeneratedPluginRegistrant.register(with: self)
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    print("DART_DEFINES: \(dartDefinesString)")
+
     var dartDefinesDictionary = [String: String]()
     for definedValue in dartDefinesString.components(separatedBy: ",") {
       guard let decodedData = Data(base64Encoded: definedValue),
         let decoded = String(data: decodedData, encoding: .utf8)
       else { continue }
+
       let values = decoded.components(separatedBy: "=")
       if values.count == 2 {
         dartDefinesDictionary[values[0]] = values[1]
       }
     }
+
     if let apiKey = dartDefinesDictionary["MAPS_API_KEY"] {
+      print("Found MAPS_API_KEY: \(apiKey)")
       GMSServices.provideAPIKey(apiKey)
     } else {
-      print("MAPS_API_KEY not found in dart defines!")
+      print("ERROR: MAPS_API_KEY not found in dart defines!")
     }
 
-    GMSServices.provideAPIKey(dartDefinesDictionary["MAPS_API_KEY"]!)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
